79f34ccb0f8bb24fe1317eec793793515cdf178a
MaximoGismondi <maximogismondi@gmail.com>
2024-12-12 12:45:08 -0300

    Base cases for user CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express, { Request, Response } from "express";
+
+import userRoutes from "./routes/users";
+// import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello, TypeScript with Express!");
+  res.send("Hello, TypeScript with Express!");
 });
 
+// app.use(bodyParser.json());
+app.use("/users", userRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
