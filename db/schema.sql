b175d939f8ea9ca9e7b89956019e3c44b71b24de
MaximoGismondi <maximogismondi@gmail.com>
2025-04-03 12:06:29 -0300

    Feat: Database table exist validation and maintenance tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 -- Connect to the database
-CREATE TABLE vehicles (
+CREATE TABLE IF NOT EXISTS vehicles (
     id bigint primary key generated always as identity,
     license_plate text not null unique,
     brand text not null,
@@ -8,8 +8,10 @@ CREATE TABLE vehicles (
     img_url text not null
 );
 
+
+
 -- Create the users table
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id bigint primary key generated always as identity,
     first_name text not null,
     last_name text not null,
@@ -17,16 +19,20 @@ CREATE TABLE users (
     email text not null unique
 );
 
+
+
 -- Create the assignments table
-CREATE TABLE assignments (
+CREATE TABLE IF NOT EXISTS assignments (
     id bigint primary key generated always as identity,
     user_id bigint not null references users (id),
     vehicle_id bigint not null references vehicles (id),
     unique (user_id, vehicle_id)
 );
 
+
+
 -- Create the reservations table
-CREATE TABLE reservations (
+CREATE TABLE IF NOT EXISTS reservations (
     id bigint primary key generated always as identity,
     user_id bigint not null references users (id),
     vehicle_id bigint not null references vehicles (id),
@@ -34,3 +40,44 @@ CREATE TABLE reservations (
     end_date date not null,
     recurrence_pattern text
 );
+
+
+
+-- Create the maintenance categories table
+CREATE TABLE IF NOT EXISTS maintenance_categories (
+    id bigint primary key generated always as identity,
+    name text not null
+);
+
+
+
+-- Create the maintenance table
+CREATE TABLE IF NOT EXISTS maintenance (
+    id bigint primary key generated always as identity,
+    category_id bigint not null references maintenance_categories (id),
+    name text not null,
+    img_name text not null
+)
+
+
+
+-- Create the assigned maintenance table
+CREATE TABLE IF NOT EXISTS assigned_maintenance (
+    id bigint primary key generated always as identity,
+    vehicle_id bigint not null references vehicles (id),
+    maintenance_id bigint not null references maintenance (id),
+    kilometers_frequency integer not null,
+    recurrence_pattern text
+)
+
+
+
+-- Create the maintenance history table
+CREATE TABLE IF NOT EXISTS maintenance_history (
+    id bigint primary key generated always as identity,
+    assigned_maintenance_id bigint not null references assigned_maintenance (id),
+    user_id bigint not null references users (id),
+    date date not null,
+    kilometers integer not null,
+    observations text
+)
